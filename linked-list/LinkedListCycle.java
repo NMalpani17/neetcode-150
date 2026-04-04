e4dc69feacc4c63a6a729a16bc79588108b495b4
Niraj Malpani <nirajkrmalpani@gmail.com>
2026-04-04 09:30:08 -0400

    solve: linked-list-cycle

---
+++ linked-list/LinkedListCycle.java
@@ -0,0 +1,18 @@
+// Problem  : Linked List Cycle
+// Link     : https://leetcode.com/problems/linked-list-cycle/
+// Difficulty: Easy
+// Pattern  : Fast & Slow Pointers
+// Time     : O(n) | Space: O(1)
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
